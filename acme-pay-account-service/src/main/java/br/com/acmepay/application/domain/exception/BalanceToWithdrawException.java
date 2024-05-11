6bb19311b57277404a33918277aad91c5e7d3a95
caio86 <caioluiz86@gmail.com>
2024-05-11 10:37:39 -0300

    create module acme-pay-account-sevice

---
+++ acme-pay-account-service/src/main/java/br/com/acmepay/application/domain/exception/BalanceToWithdrawException.java
@@ -0,0 +1,8 @@
+package br.com.acmepay.application.domain.exception;
+
+public class BalanceToWithdrawException extends Exception {
+
+    public BalanceToWithdrawException(String message){
+        super(message);
+    }
+}
---
+++ acme-pay-account-service/src/main/java/br/com/acmepay/application/domain/models/AccountDomain.java
@@ -0,0 +1,41 @@
+package br.com.acmepay.application.domain.models;
+
+import br.com.acmepay.application.domain.exception.BalanceToWithdrawException;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AccountDomain {
+
+    private Long id;
+    private Integer number;
+    private Integer agency;
+    private BigDecimal balance;
+    private Boolean close;
+    private LocalDateTime created_at;
+    private LocalDateTime updated_at;
+
+    public void create() {
+
+    }
+
+    public void deposit(BigDecimal amount){
+        this.balance.add(amount);
+    }
+
+    public void withdraw(BigDecimal amount) throws BalanceToWithdrawException {
+        if (this.balance.compareTo(amount) >= 0){
+            this.balance.subtract(amount);
+        }else {
+            throw new BalanceToWithdrawException("error withdraw");
+        }
+
+    }
+
+}
