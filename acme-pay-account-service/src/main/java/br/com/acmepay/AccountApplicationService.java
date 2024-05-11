0ec094cdda056152ec143ce896f41a3803e9000d
caio86 <caioluiz86@gmail.com>
2024-05-11 15:31:00 -0300

    edit account-service main class

---
+++ acme-pay-account-service/src/main/java/br/com/acmepay/AccountApplicationService.java
@@ -0,0 +1,11 @@
+package br.com.acmepay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountApplicationService {
+    public static void main(String[] args) {
+        SpringApplication.run(AccountApplicationService.class, args);
+    }
+}
