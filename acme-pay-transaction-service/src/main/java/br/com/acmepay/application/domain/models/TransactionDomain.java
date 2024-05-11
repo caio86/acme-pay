34996bedc285c68f9f37e30b814e8a51fc111a4d
caio86 <caioluiz86@gmail.com>
2024-05-11 10:51:16 -0300

    create module transaction

---
+++ acme-pay-transaction-service/src/main/java/br/com/acmepay/application/domain/models/TransactionDomain.java
@@ -0,0 +1,21 @@
+package br.com.acmepay.application.domain.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TransactionDomain {
+    private Long id;
+    private LocalDateTime data_transaction;
+    private Integer source_account;
+    private Integer destination_account;
+    private BigDecimal amount;
+    private LocalDateTime created_at;
+    private LocalDateTime updated_at;
+}
