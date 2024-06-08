e59f8387b5539e08361b3e79afc34fb204198ee4
caio86 <caioluiz86@gmail.com>
2024-06-08 13:26:33 -0300

    create custemer application entrance

---
+++ acme-pay-customer-service/src/main/java/br/com/acmepay/CustomerApplicationService.java
@@ -0,0 +1,11 @@
+package br.com.acmepay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerApplicationService {
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerApplicationService.class, args);
+    }
+}
