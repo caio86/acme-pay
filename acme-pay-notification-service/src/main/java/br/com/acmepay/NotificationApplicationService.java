7c0106dc6f8290d9ddadc542cbe92daa6effea0c
caio86 <caioluiz86@gmail.com>
2024-06-08 21:04:43 -0300

    create basic notification service structure

--- acme-pay-notification-service/src/main/java/br/com/acmepay/NotificationApplicationService.java
+++ acme-pay-notification-service/src/main/java/br/com/acmepay/NotificationApplicationService.java
@@ -1,7 +1,11 @@
 package br.com.acmepay;
 
-public class Main {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationApplicationService {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(NotificationApplicationService.class, args);
     }
 }
--- acme-pay-notification-service/src/main/java/br/com/acmepay/application/domain/models/NotificationDomain.java
+++ acme-pay-notification-service/src/main/java/br/com/acmepay/application/domain/models/NotificationDomain.java
@@ -1,6 +1,7 @@
 package br.com.acmepay.application.domain.models;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -10,7 +11,9 @@ import java.time.LocalDateTime;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class NotificationDomain {
+
     private Long id;
     private LocalDateTime data_transaction;
     private Integer source_account;
