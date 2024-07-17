4c63fbf660fe58ea615cd7c336b8e7646948e598
caio86 <caioluiz86@gmail.com>
2024-07-17 16:37:41 -0300

    add dependencies, modify Main.java and add resources

---
+++ acme-pay-transaction-service/src/main/java/br/com/acmepay/TransactionApplicationService.java
@@ -0,0 +1,11 @@
+package br.com.acmepay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionApplicationService {
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionApplicationService.class, args);
+    }
+}
