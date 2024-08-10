c6f0fb76297dd7bcff5a373205541b3822ba350c
caio86 <caioluiz86@gmail.com>
2024-08-09 23:44:14 -0300

    make ProducerConfig to use kafka brokers const

--- acme-pay-account-service/src/main/java/br/com/acmepay/adapters/output/queue/kafka/ProducerConfiguration.java
+++ acme-pay-account-service/src/main/java/br/com/acmepay/adapters/output/queue/kafka/ProducerConfiguration.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
@@ -12,17 +13,17 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 
-/**
- * ProducerConfiguration
- */
 @Configuration
 public class ProducerConfiguration {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String kafkaServer;
 
     @Bean
     public Map<String, Object> producerConfigs() {
         Map<String, Object> configs = new HashMap<>();
 
-        configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaServer);
         configs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
 
