4ce91e7f92902bae8a93e4536a69ac201757512b
caio86 <caioluiz86@gmail.com>
2024-08-09 23:43:32 -0300

    create kafkaAdmin configuration

--- acme-pay-account-service/src/main/java/br/com/acmepay/adapters/output/queue/kafka/TopicConfiguration.java
+++ acme-pay-account-service/src/main/java/br/com/acmepay/adapters/output/queue/kafka/TopicConfiguration.java
@@ -1,15 +1,30 @@
 package br.com.acmepay.adapters.output.queue.kafka;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaAdmin;
 
-/**
- * TopicConfiguration
- */
 @Configuration
 public class TopicConfiguration {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String kafkaServer;
+
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        Map<String, Object> configs = new HashMap<>();
+
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaServer);
+
+        return new KafkaAdmin(configs);
+    }
 
     @Bean
     public NewTopic transactions() {
