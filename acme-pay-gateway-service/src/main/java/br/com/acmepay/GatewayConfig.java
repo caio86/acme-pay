12e7d89716d154079feccf8898e459058d573dfa
caio86 <caioluiz86@gmail.com>
2024-08-13 18:46:28 -0300

    feat(gateway): implement eureka client

--- acme-pay-gateway-service/src/main/java/br/com/acmepay/GatewayConfig.java
+++ acme-pay-gateway-service/src/main/java/br/com/acmepay/GatewayConfig.java
@@ -12,13 +12,13 @@ public class GatewayConfig {
     public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route("accounts", r -> r.path("/api/v1/accounts/**")
-                        .uri("http://localhost:8081"))
+                        .uri("http://account-service:8080"))
                 .route("customers", r -> r.path("/api/v1/customers/**")
-                        .uri("http://localhost:8082"))
+                        .uri("http://customer-service:8080"))
                 .route("notifications", r -> r.path("/api/v1/notifications/**")
-                        .uri("http://localhost:8083"))
+                        .uri("http://notification-service:8080"))
                 .route("transactions", r -> r.path("/api/v1/transactions/**")
-                        .uri("http://localhost:8084"))
+                        .uri("http://transaction-service:8080"))
                 .build();
     }
 
