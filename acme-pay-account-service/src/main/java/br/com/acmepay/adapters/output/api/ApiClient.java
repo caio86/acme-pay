465ba3cda700e0de272be8f97e26c31a30c244ad
caio86 <caioluiz86@gmail.com>
2024-08-13 19:12:04 -0300

    feat(account): implement eureka client

--- acme-pay-account-service/src/main/java/br/com/acmepay/adapters/output/api/ApiClient.java
+++ acme-pay-account-service/src/main/java/br/com/acmepay/adapters/output/api/ApiClient.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "customers", url = "http://localhost:8082")
+@FeignClient(name = "customers", url = "http://customer-service:8080")
 public interface ApiClient {
 
     @GetMapping("/api/v1/customers/salary/{document}")
